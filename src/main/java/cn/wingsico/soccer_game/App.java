35178f1adf70250feeea78ee98de182343fc84d5
wingsico <wingsico@outlook.com>
2018-05-15 23:34:45 +0800

    update(Thread): 删除生成器线程

--- src/main/java/cn/wingsico/soccer_game/App.java
+++ src/main/java/cn/wingsico/soccer_game/App.java
@@ -1,29 +1,11 @@
 package cn.wingsico.soccer_game;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
-
-  @Autowired
-  private static FakeMockGenerator fakeMockGenerator;
-
   public static void main(String[] args) {
-    init();
     SpringApplication.run(App.class, args);
-  }
-
-  public static class MockThread extends Thread {
-    @Override
-    public void run() {
-      fakeMockGenerator.run();
-    }
-  }
-
-  public static void init() {
-    MockThread mockThread = new MockThread();
-    mockThread.run();
   }
 }
