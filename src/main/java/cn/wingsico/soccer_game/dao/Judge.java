9e8b934c2ed457b3a983e42bafcdf239ed1b5997
wingsico <wingsico@outlook.com>
2018-05-15 23:38:24 +0800

    feat(Judge): 新增裁判实体、裁判DAO

--- src/main/java/cn/wingsico/soccer_game/dao/Judge.java
+++ src/main/java/cn/wingsico/soccer_game/dao/Judge.java
@@ -1,4 +1,26 @@
 package cn.wingsico.soccer_game.dao;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
 public class Judge {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  private String name;
+
+  /**
+   * type: 0 assistJudge
+   * type: 1 mainJudge
+   */
+  private Integer type;
+
+  private String sex;
 }
--- src/main/java/cn/wingsico/soccer_game/dao/JudgeRepo.java
+++ src/main/java/cn/wingsico/soccer_game/dao/JudgeRepo.java
@@ -1,4 +1,6 @@
 package cn.wingsico.soccer_game.dao;
 
-public interface JudgeRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JudgeRepo extends JpaRepository<Judge, Integer> {
 }
