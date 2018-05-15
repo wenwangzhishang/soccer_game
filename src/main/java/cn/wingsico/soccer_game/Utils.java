fdf1a59f000bd0b2c8f3d4649a47a7de7d57e2c9
wingsico <wingsico@outlook.com>
2018-05-15 14:49:36 +0800

    feat(getRandomName): 生成随机姓名

--- src/main/java/cn/wingsico/soccer_game/Utils.java
+++ src/main/java/cn/wingsico/soccer_game/Utils.java
@@ -1,4 +1,26 @@
 package cn.wingsico.soccer_game;
 
+import java.util.Random;
+
 public class Utils {
+  private static Random random = new Random();
+
+  public static String getRandomName(String sex) {
+    return getRandomLastName() + " " +getRandomFirstName(sex);
+  }
+
+  public static String getRandomLastName() {
+    return StaticData.LAST_NAME[random.nextInt(StaticData.LAST_NAME_LENGTH)];
+  }
+
+  public static String getRandomFirstName(String sex) {
+    String firstName;
+    if (sex.equals("f")) {
+      firstName = StaticData.FEMALE_FIRST_NAME[random.nextInt(StaticData.FEMALE_FIRST_NAME_LENGTH)];
+    } else {
+      firstName = StaticData.MALE_FIRST_NAME[random.nextInt(StaticData.MALE_FIRST_NAME_LENGTH)];
+    }
+
+    return firstName;
+  }
 }
