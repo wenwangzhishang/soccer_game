f4f69375992c9a1d4335d71fdb75ae6001171052
wingsico <wingsico@outlook.com>
2018-05-15 23:30:34 +0800

    update(GeneratedValue): strategy changed: AUTO -> IDENTITY

--- src/main/java/cn/wingsico/soccer_game/dao/Team.java
+++ src/main/java/cn/wingsico/soccer_game/dao/Team.java
@@ -14,7 +14,7 @@ import java.util.Set;
 @Entity
 public class Team {
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
 
   private String name;
